7dfa5dfe35daaef833e60fa538b7e754f13b078a
Aznavour <vic00tor@gmail.com>
2020-02-08 19:15:31 -0500

    ajout de l'objet que je vais animer et parent a la section global

--- js/objetAnimer.class.js
+++ js/objetAnimer.class.js
@@ -2,5 +2,21 @@ class oRectangleanimer{
     constructor(hauteur,largeur){
         this.hauteur = hauteur;
         this.largeur = largeur;
+
+        this.dessineRectangle(this.hauteur,this.largeur);
+    }
+
+
+
+    dessineRectangle(hauteur,largeur){
+        var oSectionGlobal = document.querySelectorAll(".global")[1];
+        
+        console.log(oSectionGlobal);
+        var oRectangle = document.createElement("div");
+        oRectangle.style.width = largeur + "px";
+        oRectangle.style.height = hauteur + "px";
+        oRectangle.style.backgroundColor = "black";
+        oRectangle.classList.add("rectangleIntro");
+        oSectionGlobal.appendChild(oRectangle);
     }
 }
